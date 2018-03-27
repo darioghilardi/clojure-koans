262a795a12ac7985dd333b7f03729a1073b6e57a
Dario Ghilardi <darioghilardi@webrain.it>
2018-03-27 12:57:25 +0200

    Complete the koan on recursion.

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,25 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (reverse coll))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         a 1]
+    (if (= n 1)
+      a
+      (recur (dec n) (* a n)))))
 
 (meditations
   "Recursion ends with a base case"
